5ca3cd0f5b15150b5dab4e24984288cd0dee48b5
Nabz <bnnabiswa@gmail.com>
2020-05-04 16:33:02 +0300

    install flask login, import it to views.py

--- manage.py
+++ manage.py
@@ -17,7 +17,7 @@ def test():
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app,db = db,User = User,Role = Role )
+    return dict(app = app,db = db,User = User)
 
 migrate = Migrate(app,db)
 manager.add_command('db',MigrateCommand)
