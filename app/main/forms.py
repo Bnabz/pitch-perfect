f82603e21f4fe0ed15e229ff18221cd5c0f06e2a
Nabz <bnnabiswa@gmail.com>
2020-05-05 07:12:51 +0300

    add flask forms in forms.py

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,8 +1,23 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,TextAreaField,SubmitField
+from wtforms import StringField,TextAreaField,SubmitField,SelectField
 from wtforms.validators import Required
 
 
 class UpdateProfile(FlaskForm):
     bio = TextAreaField('Tell us about you.',validators = [Required()])
     submit = SubmitField('Submit')
+
+
+class PostForm(FlaskForm):
+    title = StringField('Title', validators=[Required()])
+    post = TextAreaField('Your pitch', validators=[Required()])
+    category = SelectField('Category', choices=[('product', 'product'), ('pickup line', 'pickup line'), ('business', 'business')],
+                           validators=[Required()])
+    submit = SubmitField('Post')
+
+
+class CommentForm(FlaskForm):
+    comment = TextAreaField('Post a comment', validators=[Required()])
+    submit = SubmitField('Post')
+
+
