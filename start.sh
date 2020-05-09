5e941dad1ccfa931fb5b67c52d2f1084c6514de3
Nabz <bnnabiswa@gmail.com>
2020-05-09 14:28:02 +0300

    update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
-export MAIL_USERNAME='bnnabiswa@gmail.com'
-export MAIL_PASSWORD='demokrasi'
+export MAIL_USERNAME='brnabiswa@gmail.com'
+export MAIL_PASSWORD=
 export SECRET_KEY='12345'
 
 python3.8 manage.py server
